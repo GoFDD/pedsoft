d0cda6183e92fc1c9c85f985043ca7add553f48c
GoFDD <juliocesargolfredo2022@gmail.com>
2024-05-10 21:32:55 -0300

    Teste

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<link rel="stylesheet" href="./src/assets/css/style.css">
+	
+	<div class="app">
+		
+
+		<!-- Sidebar -->
+		<Sidebar />
+
+
+		<!-- Content -->
+		<router-view />
+	</div>
+</template>
+
+<script setup>
+import Sidebar from './components/Sidebar.vue'
+</script>
+
+<style lang="scss">
+
+</style>
